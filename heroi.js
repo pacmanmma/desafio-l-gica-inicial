867147df8f642ebefdc6d14630ee4daab5adcd06
pacmanmma <guaxinim545@gmail.com>
2025-04-04 15:53:37 -0300

    terminado o projetinho

--- heroi.js
+++ heroi.js
@@ -1,20 +1,25 @@
-let nomeHeroi = "pacmaan";
-let xpHeroi = 512000;
+let nomeHeroi = "Superman";
+let xpHeroi = 700;
+let nivelHeroi = "";
 
-if(xpHeroi >= 1.000 && xpHeroi <= 2000){
-    console.log("Nivel Ferro");
+if(xpHeroi < 1000){
+    nivelHeroi = "Ferro";
+} else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    nivelHeroi = "Bronze";
 } else if(xpHeroi >= 2001 && xpHeroi <= 5000){
-    console.log("Nivel Prata");
-} else if(xpHeroi >= 6001 && xpHeroi <= 7000){
-    console.log("Nivel Ouro");
-} else if(xpHeroi >= 7001 && xpHeroi <= 7000){
-    console.log("Nivel Platina Diamante");
+    nivelHeroi = "Prata";
+} else if(xpHeroi >= 5001 && xpHeroi <= 7000){
+    nivelHeroi = "Ouro";
+} else if(xpHeroi >= 7001 && xpHeroi <= 8000){
+    nivelHeroi = "Platina";
 } else if(xpHeroi >= 8001 && xpHeroi <= 9000){
-    console.log("Nivel Ascendente");
+    nivelHeroi = "Ascendente";
 } else if(xpHeroi >= 9001 && xpHeroi <= 10000){
-    console.log("Nivel Imortal");
-}else if(xpHeroi > 10001){
-    console.log("Nivel Radiant");
-} else {
-    console.log("Nivel Iniciante");
+    nivelHeroi = "Imortal";
+} else if(xpHeroi > 10000){
+    nivelHeroi = "Radiante";
+} else{
+    console.log("NOOOOBB")
 }
+
+console.log(`O heroi ${nomeHeroi} tem ${xpHeroi} de XP e é ${nivelHeroi}`);
